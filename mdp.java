b4a7646fed2a968892c283be58a7b0894688340b
mageezy <56899818+mageezy@users.noreply.github.com>
2020-02-10 15:06:27 -0500

    added things we discussed last night
    removed value from a state object.
    added coords to a state object.
    getters and setters for state objects.
    added global constants to mdp class.
    put transition and reward function in mdp class.

--- mdp.java
+++ mdp.java
@@ -1,6 +1,27 @@
 public class mdp {
+
+    public static double Discount_Factor = .99;
+    public static double Max_Error = 0;
+    public static double Pos_Reward = 1;
+    public static double Neg_Reward = -1;
+    public static double Step_Cost = -.04;
+    public static double Forward_Prob = .8;
+    public static double Clockwise_Prob = .1;
+    public static double Counter_Prob = .1;
+    private static double[][][] Rewards = new double[65][65][4];
+    private static double[][][] Transition = new double[65][65][4];
+
     public static void main(String [] args) {
-        State rando = new State(1, 'E', 0.4);
+        State rando = new State(1, 'N', 1, 1, 1);
         //System.out.println("it do something");
     }
+
+    public static double reward(State start, char action, State next) {
+        return 0;
+    }
+
+    public static double transition(State start, char action, State next) {
+        return 0;
+    }
+
 }

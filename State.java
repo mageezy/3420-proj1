4527a04a0379852ca33f59677ba4e66b25452c82
mageezy <56899818+mageezy@users.noreply.github.com>
2020-02-10 00:56:46 -0500

    more stuff

--- State.java
+++ State.java
@@ -4,6 +4,12 @@ public class State {
     private char optMove;
     private double value;
 
+    public State(int stateNum) {
+        this.stateNum = stateNum;
+        this.optMove = '\0';
+        this.value = -Double.MAX_VALUE;
+    }
+
     public State(int stateNum, char optMove, double value) {
         this.stateNum = stateNum;
         this.optMove = optMove;
@@ -13,8 +19,14 @@ public class State {
     public int getStateNum() {
         return stateNum;
     }
+    public void setOptMove(char move) {
+        this.optMove = move;
+    }
     public char getOptMove() {
         return optMove;
+    }
+    public void setValue(double value) {
+        this.value = value;
     }
     public double getValue() {
         return value;
--- mdp.java
+++ mdp.java
@@ -1,5 +1,6 @@
 public class mdp {
     public static void main(String [] args) {
-        System.out.println("it do something");
+        State rando = new State(1, 'E', 0.4);
+        //System.out.println("it do something");
     }
 }

1508462655cb08370dce6095aa6ef0ce3b09dc1e
Caleb Eurich <calebeurich27@gmail.com>
2020-02-10 22:25:08 -0500

    added notes

--- mdp.java
+++ mdp.java
@@ -1,5 +1,16 @@
 public class mdp {
 
+//have 3d array of state objects to generate transition function
+//have 1d array of state values to reference by state number
+//780 line hard code transition function to assign all answers to possible transitions
+//we can write a transition function to actually figure this out with our positional array
+//reward function is is also a refferencable 3d array that is usually just the step cost unless it is a +1 or -1
+//write function to build 3d array of transition function answers instead of hard coding
+//add a get coords function of the node by node number
+//also add its North, West, East, South nodes as properties when building the nodes for easy reference in building transition function table
+//DO NOT NEED to make actual value iteration calculations. Just the initial step stuff
+
+
     public static double Discount_Factor = .99;
     public static double Max_Error = 0;
     public static double Pos_Reward = 1;

c3e59517b9837f7f50c6c69f9407fccab087a3fb
Caleb Eurich <calebeurich27@gmail.com>
2020-02-10 22:55:02 -0500

    added more hardcode to move

--- State.java
+++ State.java
@@ -1,5 +1,38 @@
 public class State {
 
+//delete this crap later
+
+Game_Board[2][0][0] = new State(40, 2, 0, 0);
+Game_Board[2][0][1] = new State(41, 2, 0, 1);
+Game_Board[2][1][0] = new State(42, 2, 1, 0);
+Game_Board[2][1][1] = new State(43, 2, 1, 1);
+Game_Board[2][2][0] = new State(44, 2, 2, 0);
+Game_Board[2][2][1] = new State(45, 2, 2, 1);
+Game_Board[2][3][0] = new State(46, 2, 3, 0);
+Game_Board[2][3][1] = new State(47, 2, 3, 1);
+Game_Board[2][4][0] = new State(48, 2, 4, 0);
+Game_Board[2][4][1] = new State(49, 2, 4, 1);
+Game_Board[1][0][0] = new State(50, 1, 0, 0);
+Game_Board[1][0][1] = new State(51, 1, 0, 1);
+Game_Board[1][1][0] = new State(52, 1, 1, 0);
+Game_Board[1][1][1] = new State(53, 1, 1, 1);
+Game_Board[1][2][0] = new State(54, 1, 2, 0);
+Game_Board[1][2][1] = new State(55, 1, 2, 1);
+Game_Board[1][3][0] = new State(56, 1, 3, 0);
+Game_Board[1][3][1] = new State(57, 1, 3, 1);
+Game_Board[0][0][0] = new State(58, 0, 0, 0);
+Game_Board[0][0][1] = new State(59, 0, 0, 1);
+Game_Board[0][1][0] = new State(60, 0, 1, 0);
+Game_Board[0][1][1] = new State(61, 0, 1, 1);
+Game_Board[0][2][0] = new State(62, 0, 2, 0);
+Game_Board[0][2][1] = new State(63, 0, 2, 1);
+Game_Board[0][3][0] = new State(64, 0, 3, 0);
+
+
+
+
+
+
     private int stateNum;
     private char optMove;
     private int[] coords = new int[3];
